78b5b7c51b50334308a51f5b286d8ce0d5386aff
Jayesh <jaygangi27@gmail.com>
2026-04-20 22:51:55 +0530

    Initial commit, project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setup import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jayesh Gangi",
+    author_email = "jaygangi27@gmail.com",
+    packages = find_packages()
+)
