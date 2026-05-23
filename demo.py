9f180ffd4d28dc7fdb62c8af68be32376f064d79
Jayesh <jaygangi27@gmail.com>
2026-05-23 23:28:23 +0530

    Data ingestion added

--- demo.py
+++ demo.py
@@ -1,8 +1,13 @@
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1 + 'Z'
-except Exception as e:
-    MyException(e,sys)
+# try:
+#     a = 1 + 'Z'
+# except Exception as e:
+#     MyException(e,sys)
+
+from src.pipeline.training_pipeline import TrainPipeline
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
